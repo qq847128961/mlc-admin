35235a14223c440b7470270bc10fd3a13da9d246
mlc <maliuchou@vanmall.net>
2021-01-03 12:44:01 +0800

    [A]

---
+++ 47fcebd5fb8824e99bb8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{262:function(e,t,n){"use strict";n.r(t);var c={fetch:function(e){(0,e.redirect)("/user/list")}},l=n(26),component=Object(l.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"page-admin-index"})}),[],!1,null,null,null);t.default=component.exports}}]);
